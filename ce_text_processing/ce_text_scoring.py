897e6181bb31db92ecbf5df1dea4326e25093389
Hamza JEDDAD <hamza.jeddad@student.ecp.fr>
2019-01-22 21:40:54 +0100

    add compare method

--- ce_text_processing/ce_text_scoring.py
+++ ce_text_processing/ce_text_scoring.py
@@ -33,6 +33,9 @@ class TextScoring():
         return TextScoring.bov.score(sent1, sent2)
 
 if __name__=="__main__":
-    text1 = "Je n'arrive pas me connecter au serveur de données"
-    text2 = "Il arrive as ne pas se connecter au choses aléatoires des données"
+    text1 = "Dans une copropriété calme et sécurisée à deux pas des transports et commerces, nous vous proposons un trois pièces de 68m² il comporte une entrée, un grand séjour, deux chambres, une cuisine, et un WC indépendant, une cave et un parking couvert complètent également ce bien."
+    text2 = "Appartement 3 pièces 68 m²plus parking cave , double vitrage équipé, (7e étage) vue dégagée sur espace vert avec ascenseur.Une cave et un parking.Situé à 2 min du métro (ligne 7) et du tramway (T3) station Porte de Choisy et à 5 min de la station Maison Blanche (future ligne 14).Bus, commerces et restaurants à proximité.Appartement comprenant une entrée avec placard aménagé, 2 chambres, un séjour de 22 m², une salle de bain, un wc séparé et une cuisine séparée.le chauffage collectif, et surveillance gardien 24h/24.Plus de photos sur demande.Si vous êtes intéressé, appeler ou laisser un message."
+    text3 = "MURAT MOLITOR. Votre agence Century 21 Via conseil vous propose dans un bel immeuble pierre de taille 1930, avec gardien, au 5ème étage ascenseur, beau 3 pièces comprenant salon, salle à manger, chambre (2 possibles), cuisine, salle d'eau, wc séparé, rangement, cave. Traversant, bien distribué, bonne hauteur sous plafond, charme de l'ancien, lumineux. Proche des transports et commerces."
     print(TextScoring.get_score(text1,text2))
+    print(TextScoring.get_score(text1,text3))
+    print(TextScoring.get_score(text2,text3))
--- flaskr/global_comparator.py
+++ flaskr/global_comparator.py
@@ -1,11 +1,49 @@
-
+from ce_text_processing.ce_text_scoring import TextScoring
+from imageComparator import histogram,SSIM
 
 class GlobalComparator:
     """Class for comparisons of ads"""
 
+    weight_text = 0.05
+    weight_image = 0.4
+    weight_surface = 0.25
+    weight_rooms = 0.2
+    weigh_price = 0.1
 
-
-
+    def __compare_(n1, n2):
+        if n1 == n2 == 0 :
+            return 0
+        return 1-abs(n1-n2)/max(n1,n2)
 
     @staticmethod
     def __compare(ad1, ad2):
+        score_text = weight_text * TextScoring.get_score(ad1['text'],ad2["text"])
+        max_score_image = 0
+        for image1 in ad1["image"] :
+            for image2 in ad2["image"] :
+                image_ssim = SSIM(image1,image2)
+                image_score_ssim = image_ssim.compare_images()[1]
+                if max_score_image < image_score_ssim :
+                    max_score_image = image_score_ssim
+
+                image_hist = histogram(ad1["image"],ad2["image"])
+                image_score_hist = image_hist.correlation()
+                if max_score_image < image_score_hist :
+                    max_score_image = image_score_hist
+        score_image = weight_image*max_score_image
+        if ad1["price"] != "NC" and ad2["price"] != "NC" :
+            score_price = weigh_price * __compare_(ad1["price"],ad2["price"])
+        else :
+            score_price = 0
+
+        if ad1["rooms"] != "NC" and ad2["rooms"] != "NC" :
+            score_rooms = weigh_rooms * __compare_(ad1["rooms"],ad2["rooms"])
+        else :
+            score_rooms = 0
+
+        if ad1["surface"] != "NC" and ad2["surface"] != "NC" :
+            score_surface = weigh_surface * __compare_(ad1["surface"],ad2["surface"])
+        else :
+            score_surface = 0
+
+        return score_rooms + score_text + score_price + score_surface + score_image
