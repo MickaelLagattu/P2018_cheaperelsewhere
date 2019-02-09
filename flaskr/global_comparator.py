133ef6874fb6e92cb6f20b25f626f35d035710a3
Mickael Lagattu <mickael.lagattu18@gmail.com>
2019-02-09 20:14:43 +0100

    correction typo

--- flaskr/global_comparator.py
+++ flaskr/global_comparator.py
@@ -22,15 +22,15 @@ class GlobalComparator:
         if type(surface) == int and place != 'NC':
             surface_min, surface_max = surface*0.8, surface*1.2
             potential_similar = mongo.db.ads.find(
-                {'location': place, 'surface': {"$gt": surface_min, "$lt": surface_max}, 'agency': {'$ne': ad['site']}})
+                {'location': place, 'surface': {"$gt": surface_min, "$lt": surface_max}, 'agency': {'$ne': ad['agency']}})
         elif place != "NC":
-            potential_similar = mongo.db.ads.find({'location': place, 'agency': {'$ne': ad['site']}})
+            potential_similar = mongo.db.ads.find({'location': place, 'agency': {'$ne': ad['agency']}})
         elif type(surface) == int:
             surface_min, surface_max = surface * 0.8, surface * 1.2
             potential_similar = mongo.db.ads.find(
-                {'surface': {"$gt": surface_min, "$lt": surface_max}, 'agency': {'$ne': ad['site']}})
+                {'surface': {"$gt": surface_min, "$lt": surface_max}, 'agency': {'$ne': ad['agency']}})
         else:
-            potential_similar = list(mongo.db.ads.find({'agency': {'$ne': ad['site']}}))
+            potential_similar = list(mongo.db.ads.find({'agency': {'$ne': ad['agency']}}))
 
         similar = []
         print("avec les annonces", potential_similar)
