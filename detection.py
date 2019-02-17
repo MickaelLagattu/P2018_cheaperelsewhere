e3b04ac4bc9b5b1244edef90d6ea0fe4d6d9b27b
noubair.m@gmail.com <noubair.m@gmail.com>
2019-02-17 15:46:49 +0100

    removed prints and added some comments

--- detection.py
+++ detection.py
@@ -4,13 +4,10 @@ from imageai.Detection import ObjectDetection
 class detection:
     def __init__(self, image):
         full_path = os.path.realpath(__file__)
-        # print("full_path",full_path)
         full_path = str.replace(full_path, '\\', '/')
-        # print("full_path", full_path)
         path, filename = os.path.split(full_path)
         self.image=image
         self.execution_path = path + "/" + "flaskr/static/images/"
-        # print(self.execution_path)
         self.path = ""
         self.detector = ObjectDetection()
         self.detector.setModelTypeAsRetinaNet()
@@ -20,12 +17,11 @@ class detection:
         self.detector.loadModel()
 
     def getObjects(self):
-        # print(self.path)
+        """ returns a list of dictionaries. Each dictionary contains the detected object,
+        its percentage of probability and coordinates of the rectangular frame of the object"""
         image_name = os.path.basename(self.image)
         objects = self.detector.detectObjectsFromImage(input_image=os.path.join(self.image),
                                                         output_image_path=os.path.join(self.execution_path,
                                                                                     self.path+"detection_output/" + "new" + image_name[:-4]))
-        # for eachObject in objects:
-        #     print(eachObject["name"], " : ", eachObject["percentage_probability"])
 
         return objects
